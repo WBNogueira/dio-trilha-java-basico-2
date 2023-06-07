0842985ad2ceb610351176214d006093981a83e3
Wagner Nogueira <wbnogueira3@gmail.com>
2023-06-07 18:55:59 -0300

    Concluido as linhas de comando da classe ContaTerminal

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        //TODO: Conhecer e importar a classe Scanner
+        
+        Scanner sc = new Scanner(System.in);
+
+        //Exibir as mensagens para o nosso usuário e obter pela classe scanner os valores digitados no terminal
+        System.out.print("Por favor, digite o número da conta! ");
+        int numero = sc.nextInt();
+        System.out.print("Por favor, digite o número da Agência! ");
+        String agencia = sc.next();
+        System.out.print("Por favor, digite o nome do cliente! ");
+        String nomeCliente = sc.next();
+        System.out.print("Por favor, digite o saldo da conta! ");
+        Double saldo = sc.nextDouble();
+
+        //Exibir a mensagem da conta criada
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque");
     }
 }
